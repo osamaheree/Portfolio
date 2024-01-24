1ce47503886f56c6a2053be1839bb505543a4268
osamaheree <goldleaf1304@gmail.com>
2024-01-25 01:52:04 +0500

    Initial commit

---
+++ .history/#TATIC/script_20231206182252.js
@@ -0,0 +1,30 @@
+ß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚üVß
+ò[A~+Èoù­ ¿ä·‚¼O÷	ò>AÞ'ÈûyŸ ïä}‚¼O÷	ò>AÞ'ÈûyŸ ïä}‚¼O÷	ò>AÞ'ÈûyŸ ïä}‚¼O÷	ò>AÞ'ÈûyŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}yŸCÞç÷9ä}Ÿ‡þçï—§þûþýrü}òÿùû(ÿÍÿ>
+þþÉÿÜüÿ×ýÁ©©\ef¾ßo–J5å*2÷¸Ž#L}¶êñ¢·$ÇÏ>~÷ŸÓÌèÙ}Vo2zZõ/¶Ä#îøLòü³OÜÿçôXõæó|ÐC¢Ïn‰ÿœžKô¹éÖ?7¾Öd|óSV_¯úËŽ®vêÔÊÜY…úü6ŸÚ8_àÙ	ï4~Ì:µâšVÖ¯zÇGàLÂÍæüÉùŒžUýFq|p¾Œ/½€ãKà"xžOê!U†Þ
+®€×ƒÓ8ßFèmàvðfÎ'Î·z¸|ýÂùvCÏBŸ­¯·kæjú^¿ÿñH9nxÚí{|TÅùÿ7Âr5Ò¨wwvN6¨˜¹‰U»¹-$šÙÑÚšÅô*õRl¿Õ¦Þ{­m±­JË·š^¾«¶þ¬ZÛ/µ?m¿–Z-ß=sæìžgžy–=þ~§¯š=ïy>3sž™gÎ™Ý³ËuuËòóò®¥^5eŽ2V…^W^ÏÓyú3»†ßó·iNvý„Ôÿó5¼tó·ýç¯ÚÆLù·„hèYçï«çeoÿ”ÔÿƒDÂ
+ÁËBµxéÑ[ìÄ6È®(íWÒã¦Ó/QûTÖf×¯8Ñùxõ¥jñ&ú3Ôâú¹jq·?ý<µøúùjñ.©§3)ß«©¥· rüwÈùSUÈªVçO)Ì¦Ïd×K=ÿùP?8.»þ‘ÅÏ3åR¯Ðjð:Ç_Hžÿ˜@Vý³êùS¼)]ÿªÚÿ¢ÂlçôGU}1Ôïï)Ëªÿ—ª\Û?Aÿ'æ)úOøÓÏVõŸô§/Sõ;ýé-U½?}­ª¿ÁŸ~ªßµZþµúfE_¬èîÌ®ïÍSç¿ÓãcÜ™ÿÅ§zõ3UýÇ}oñÎusÙiÎuöé?§ô·Ô§ûwý—ÕþOuôC²ýÁüìúû•öK`û›OÐþÔö§;ú`ÃiºñCú'TýLG_,õÞë©Nÿœ:JaÿCs²ë«ú*¨/îÏ®[ÕîÌvýAúãhþ;í^tšÛƒ¬úÉùjû{h^6ý¤G·ƒõÙôgç«ý¯’ý/o½8seN-å³TýùŠþö‹«Åß‘‘sÑí”N¿Féÿ°ÔënÇtú&µÿ›ý^Ùþe'Ð'}“Ôßïèwÿ(?«~—ÿÒZèp}ööoÏ?ÁõßscT ¹ÿ¼[ÕçûÓ? ê«Ôû·ìúåë÷0Ãmgü ÝÿŽú‘×´ztÿû¡ßÛÆtztÿû*¡ß÷šVîß"ôûÛ¢:=ºÿ}_‰¿×_Äqvv}°àó§èÑýçô}ÿG_sú_5,,hüçúà[Q”Àù<¤Qú¿;úýã¼ú|¼£úà}Ò7ž(~ùà%š¿VÛ—þCã9Ê?¾Cm_éHi »~‡Ò~©|14ç¤*8Áþ7˜]¿ÇWüòþ^"~#tñÃú‡}Åëñ™ÈsÒ?ã+~Xÿª¯øå#ýQ"~£uñÃúã¾â‡õ“õñÄsÒŸVè'~X¿¨ÐOü
+þ¼B}üŽLÒÅëWú‰Ö_BÄïÈdž“¾ÃWü°~‡¯ø"ý."~Á)ºøaým¾â‡õ÷ñóœôûŠÖð¿1Hÿ4¿²b]ü°þ¯øaý[DüÊ¦òœôÇ}Åë'ñ¿"¤Ÿ3F¿ú©ºøaý9cüÄëÑÇ¯~ÏI¿bŒŸøaý%¾â7é·ñkŸ¦‹ÖøŠÖß@Ä¯½„ç¤¿ÍWü°þ_ñ"ýCDü†JtñÃúŸøŠÖ$â74ç¤?ä+~Xÿ–¯øCúˆøL×Åë'ù‰ÖŸRDÜÿÍà9éÏ)ò?¬_\”=~Ã ~¾Aé¿ûbTö¿)/»þ’"_û´ÿÞZD\¿NÖî~À×øaýÄø•Íä9éoó5~X¯øå!ýCDüêgj÷?Hÿ_ñÃúƒDüêgñœô‡|Åëßò¿|¤ÿ€ˆ_û,íþé'ŽõµÿAúSÆ×¯Ù<'ý9c}í~ñX_û¤¯K\¿fk÷?H±¯øaý"~C§ðœô¾â‡õ7øŠ_!ÒßBÄoäíþéïò?¬ˆßÈž“þ'¾â‡õ}ÅoÒ¿LÄotŽvÿƒôoøŠÖ¿OÄoôTž“~bÐ×þ¯A_û¤?+H¼qªvÿƒô± ¯ýÒ
